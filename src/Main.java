fd931d9eead6bb02c33a4fc39c03f910be876cdf
bytrigi <adritrg28@gmail.com>
2025-10-28 19:12:57 +0100

    Se sube programa con las funciones requeridas añadidas

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+//COMENTARIO PARA HACER PUSH
+
 public class Main {
 
     public static void main(String[] args) {
